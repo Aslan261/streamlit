6ac485a38a32ede6a5b9feef2edb819cd69e1a41
Lucas Assunção <107157302+Aslan261@users.noreply.github.com>
2025-12-29 20:27:58 -0300

    Update analise_biblica.py

--- analise_biblica.py
+++ analise_biblica.py
@@ -513,7 +513,7 @@ if uploaded_file is not None:
                             
                             # Chamada ao modelo
                             response = client.models.generate_content(
-                                model='gemini-2.0-flash',
+                                model='gemini-1.5-flash',
                                 contents=prompt
                             )
                             
